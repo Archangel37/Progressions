03589553aa0d3d59b453fbed604cd3d2f89b4aaa
Archangel <djarchangel3737@gmail.com>
2017-09-01 10:22:31 +0300

    started project, added text boxes, 7 calculations for 7 progressions

---
+++ Arithmetic_Progressions/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Arithmetic_Progressions
+{
+    partial class Progressions
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox_Output = new System.Windows.Forms.RichTextBox();
+            this.button_Simple_Numbers = new System.Windows.Forms.Button();
+            this.button_Fibonacci = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Boundary = new System.Windows.Forms.TextBox();
+            this.button_Lucas = new System.Windows.Forms.Button();
+            this.button_Catalan = new System.Windows.Forms.Button();
+            this.button_Factorials = new System.Windows.Forms.Button();
+            this.button_Squares = new System.Windows.Forms.Button();
+            this.button_Central = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox_Output
+            // 
+            this.richTextBox_Output.Location = new System.Drawing.Point(12, 88);
+            this.richTextBox_Output.Name = "richTextBox_Output";
+            this.richTextBox_Output.Size = new System.Drawing.Size(1123, 589);
+            this.richTextBox_Output.TabIndex = 0;
+            this.richTextBox_Output.Text = "";
+            // 
+            // button_Simple_Numbers
+            // 
+            this.button_Simple_Numbers.Location = new System.Drawing.Point(12, 12);
+            this.button_Simple_Numbers.Name = "button_Simple_Numbers";
+            this.button_Simple_Numbers.Size = new System.Drawing.Size(140, 23);
+            this.button_Simple_Numbers.TabIndex = 1;
+            this.button_Simple_Numbers.Text = "The prime numbers";
+            this.button_Simple_Numbers.UseVisualStyleBackColor = true;
+            this.button_Simple_Numbers.Click += new System.EventHandler(this.button_Simple_Numbers_Click);
+            // 
+            // button_Fibonacci
+            // 
+            this.button_Fibonacci.Location = new System.Drawing.Point(12, 41);
+            this.button_Fibonacci.Name = "button_Fibonacci";
+            this.button_Fibonacci.Size = new System.Drawing.Size(140, 23);
+            this.button_Fibonacci.TabIndex = 2;
+            this.button_Fibonacci.Text = "Fibonacci Numbers";
+            this.button_Fibonacci.UseVisualStyleBackColor = true;
+            this.button_Fibonacci.Click += new System.EventHandler(this.button_Fibonacci_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(803, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Boundary of values";
+            // 
+            // textBox_Boundary
+            // 
+            this.textBox_Boundary.Location = new System.Drawing.Point(907, 11);
+            this.textBox_Boundary.Name = "textBox_Boundary";
+            this.textBox_Boundary.Size = new System.Drawing.Size(216, 20);
+            this.textBox_Boundary.TabIndex = 4;
+            // 
+            // button_Lucas
+            // 
+            this.button_Lucas.Location = new System.Drawing.Point(159, 11);
+            this.button_Lucas.Name = "button_Lucas";
+            this.button_Lucas.Size = new System.Drawing.Size(137, 23);
+            this.button_Lucas.TabIndex = 5;
+            this.button_Lucas.Text = "Lucas numbers";
+            this.button_Lucas.UseVisualStyleBackColor = true;
+            this.button_Lucas.Click += new System.EventHandler(this.button_Lucas_Click);
+            // 
+            // button_Catalan
+            // 
+            this.button_Catalan.Location = new System.Drawing.Point(159, 41);
+            this.button_Catalan.Name = "button_Catalan";
+            this.button_Catalan.Size = new System.Drawing.Size(137, 23);
+            this.button_Catalan.TabIndex = 6;
+            this.button_Catalan.Text = "Catalan numbers";
+            this.button_Catalan.UseVisualStyleBackColor = true;
+            this.button_Catalan.Click += new System.EventHandler(this.button_Catalan_Click);
+            // 
+            // button_Factorials
+            // 
+            this.button_Factorials.Location = new System.Drawing.Point(303, 10);
+            this.button_Factorials.Name = "button_Factorials";
+            this.button_Factorials.Size = new System.Drawing.Size(139, 23);
+            this.button_Factorials.TabIndex = 7;
+            this.button_Factorials.Text = "Factorials";
+            this.button_Factorials.UseVisualStyleBackColor = true;
+            this.button_Factorials.Click += new System.EventHandler(this.button_Factorials_Click);
+            // 
+            // button_Squares
+            // 
+            this.button_Squares.Location = new System.Drawing.Point(303, 41);
+            this.button_Squares.Name = "button_Squares";
+            this.button_Squares.Size = new System.Drawing.Size(139, 23);
+            this.button_Squares.TabIndex = 8;
+            this.button_Squares.Text = "Squares";
+            this.button_Squares.UseVisualStyleBackColor = true;
+            this.button_Squares.Click += new System.EventHandler(this.button_Squares_Click);
+            // 
+            // button_Central
+            // 
+            this.button_Central.Location = new System.Drawing.Point(448, 9);
+            this.button_Central.Name = "button_Central";
+            this.button_Central.Size = new System.Drawing.Size(166, 23);
+            this.button_Central.TabIndex = 9;
+            this.button_Central.Text = "\tCentral polygonal numbers";
+            this.button_Central.UseVisualStyleBackColor = true;
+            this.button_Central.Click += new System.EventHandler(this.button_Central_Click);
+            // 
+            // Progressions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 689);
+            this.Controls.Add(this.button_Central);
+            this.Controls.Add(this.button_Squares);
+            this.Controls.Add(this.button_Factorials);
+            this.Controls.Add(this.button_Catalan);
+            this.Controls.Add(this.button_Lucas);
+            this.Controls.Add(this.textBox_Boundary);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Fibonacci);
+            this.Controls.Add(this.button_Simple_Numbers);
+            this.Controls.Add(this.richTextBox_Output);
+            this.Name = "Progressions";
+            this.Text = "Different Progressions";
+            this.Load += new System.EventHandler(this.Progressions_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Progressions_Load(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox_Output;
+        private System.Windows.Forms.Button button_Simple_Numbers;
+        private System.Windows.Forms.Button button_Fibonacci;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Boundary;
+        private System.Windows.Forms.Button button_Lucas;
+        private System.Windows.Forms.Button button_Catalan;
+        private System.Windows.Forms.Button button_Factorials;
+        private System.Windows.Forms.Button button_Squares;
+        private System.Windows.Forms.Button button_Central;
+       
+    }
+}
+
---
+++ Arithmetic_Progressions/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Arithmetic_Progressions
+{
+    public partial class Progressions : Form
+    {
+        public Progressions()
+        {
+            InitializeComponent();
+        }
+
+
+              
+        
+        private void button_Fibonacci_Click(object sender, EventArgs e)
+        {
+            int limit = int.Parse(textBox_Boundary.Text);
+            string Fibonacci_Output = "";
+            //запись в стринговую переменную объединения массива чисел Фибоначчи через разделитель - пробел
+            Fibonacci_Output += String.Join(" ", Fibonacci(limit));
+            richTextBox_Output.Text += Fibonacci_Output + Environment.NewLine;
+        }
+
+
+        private void button_Simple_Numbers_Click(object sender, EventArgs e)
+        {
+            int limit = int.Parse(textBox_Boundary.Text);
+            string Simple_Output = "";
+            Simple_Output += String.Join(" ", SimpleNumbers(limit));
+            richTextBox_Output.Text += Simple_Output + Environment.NewLine;
+        }
+
+
+        //заполнение массива чисел Фибоначчи
+        static BigInteger[] Fibonacci(int k)
+        {
+            BigInteger[] Fib_number = new BigInteger[k+1];
+            Fib_number[0] = 0;
+            for (int i = 1; i <= k; i++)
+            {
+                if (i < 3)
+                {
+                    Fib_number[i] = 1;        
+                }
+                else
+                {
+                    Fib_number[i] = Fib_number[i - 1] + Fib_number[i - 2];        
+                }
+            }
+        return Fib_number;
+        }
+        //заполнение массива чисел Фибоначчи
+
+
+        //заполнение массива простых чисел
+        static BigInteger[] SimpleNumbers(int z)
+        {
+            BigInteger[] Simple = new BigInteger[z + 1];
+            Simple[0] = 1;
+            Simple[1] = 2;
+            for (int j = 2; j <= z; j++)
+            {
+                BigInteger temp = Simple[j - 1] + 1;
+                while (!isSimple(temp))
+                {
+                    temp++;
+                }
+                Simple[j] = temp;
+            }
+            return Simple;
+        }
+
+
+      //метод который определяет простое число или нет
+        private static bool isSimple(BigInteger N)
+        {
+            //чтоб убедится простое число или нет достаточно проверить не делится ли 
+            //число на числа до его половины, по факту - до квадратного корня, но с биг интеджерами нужно отдельный метод писать и будет это дольше
+            for (BigInteger i = 2; i <= (BigInteger)(N / 2); i++)
+            {
+                if (N % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+
+        private void button_Lucas_Click(object sender, EventArgs e)
+        {
+            //https://oeis.org/A000032
+            int limit = int.Parse(textBox_Boundary.Text);
+            string Lucas_Output = "";
+            Lucas_Output += String.Join(" ", Lucas(limit));
+            richTextBox_Output.Text += Lucas_Output + Environment.NewLine;
+        }
+
+
+        //заполнение массива чисел Лукаса
+        static BigInteger[] Lucas(int u)
+        {
+            BigInteger[] Lucas_number = new BigInteger[u + 1];
+            Lucas_number[0] = 0;
+            Lucas_number[1] = 2;
+            Lucas_number[2] = 1;
+            for (int i = 3; i <= u; i++)
+            {              
+                    Lucas_number[i] = Lucas_number[i - 1] + Lucas_number[i - 2];
+            }
+            return Lucas_number;
+        }
+
+
+        private void button_Catalan_Click(object sender, EventArgs e)
+        {
+            int limit = int.Parse(textBox_Boundary.Text);
+            string Catalan_Output = "";
+            Catalan_Output += String.Join(" ", Catalan(limit));
+            richTextBox_Output.Text += Catalan_Output + Environment.NewLine;
+        }
+
+
+        //заполнение массива чисел Каталана
+        static BigInteger[] Catalan(int p)
+        {
+            BigInteger[] Catalan_number = new BigInteger[p + 1];//тут нулевое число - базис =1, а p - это по-сути число с номером p+1
+            Catalan_number[0] = 1;
+            Catalan_number[1] = 1;
+            Catalan_number[2] = 2;
+            for (int i = 3; i <= p; i++)
+            {
+                Catalan_number[i] = (2*(2*i-1)*Catalan_number[i-1])/(i+1);
+            }
+            return Catalan_number;
+        }
+
+
+        static BigInteger[] Factorials(int y)
+        {
+            BigInteger[] Factorial = new BigInteger[y + 1];
+            Factorial[0] = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                Factorial[i] = Factorial[i-1] * i;
+            }
+            return Factorial;
+        }
+
+        private void button_Factorials_Click(object sender, EventArgs e)
+        {
+            int limit = int.Parse(textBox_Boundary.Text);
+            string Factorial_Output = "";
+            Factorial_Output += String.Join(" ", Factorials(limit));
+            richTextBox_Output.Text += Factorial_Output + Environment.NewLine;
+        }
+
+        private void button_Squares_Click(object sender, EventArgs e)
+        {
+            int limit = int.Parse(textBox_Boundary.Text);
+            string Squares_Output = "";
+            Squares_Output += String.Join(" ", Squares(limit));
+            richTextBox_Output.Text += Squares_Output + Environment.NewLine;
+        }
+
+        static BigInteger[] Squares(int y)
+        {
+            BigInteger[] Square = new BigInteger[y + 1];
+            Square[0] = 0;
+            for (int i = 1; i <= y; i++)
+            {
+                Square[i] = i * i;
+            }
+            return Square;
+        }
+
+        private void button_Central_Click(object sender, EventArgs e)
+        {
+            //	Central polygonal numbers (the Lazy Caterer's sequence): n(n+1)/2 + 1; or, maximal number of pieces formed when slicing a pancake with n cuts. 
+            //http://oeis.org/A000124
+            int limit = int.Parse(textBox_Boundary.Text);
+            string Central_Output = "";
+            Central_Output += String.Join(" ", Central(limit));
+            richTextBox_Output.Text += Central_Output + Environment.NewLine;
+        }
+
+        //без BigInteger на умножение на 70к примерно выдаёт отрицательные значения
+        static BigInteger[] Central(int y)
+        {
+           
+            BigInteger[] Centr = new BigInteger[y+1];
+            for (int i = 0; i <= y; i++)
+            {
+                Centr[i] = (new BigInteger(i) * new BigInteger(i + 1)) / 2 + 1;
+            }
+            return Centr;
+        }
+
+
+    }
+}
